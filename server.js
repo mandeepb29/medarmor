a0b6c4e6b2f73c99e8ac77701453f0797b47ac25
mandeepb29 <mandeepb29@gmail.com>
2020-02-23 23:14:32 +0530

    second commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/health-dashboard'));
+app.use(express.static('./dist/medarmor'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/health-dashboard/index.html'));
+res.sendFile(path.join(__dirname,'/dist/medarmor/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
