06b5012061c31f9bff921d86c93f8fdb1d4a66a9
mandeepb29 <mandeepb29@gmail.com>
2020-03-19 17:03:43 +0530

    health card updated and signup form - mandeep

---
+++ src/app/signup/signup.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignupComponent } from './signup.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignupComponent
+  },
+]
+
+
+@NgModule({
+  declarations: [SignupComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class SignupModule { }
